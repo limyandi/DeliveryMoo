6b3a7382ace4bda944dacc93c92e0ff0c2d93541
rico <limyandi@gmail.com>
2019-07-18 12:58:37 +1000

    skeleton node express

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import router from './routes'
+
+const app = express()
+
+// parse incoming requests data (global).
+// example of middleware.
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+//use personal router
+app.use(router)
+
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+// TODO: Different Routes here.
+
+const router = express.Router()
+
+export default router
