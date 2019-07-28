002f9ed76adc0da85b80223cac79b583d3f202c1
rico <limyandi@gmail.com>
2019-07-28 14:19:28 +1000

    fix prod username and pass

--- server/config/config.js
+++ server/config/config.js
@@ -13,8 +13,8 @@ module.exports = {
         "operatorsAliases": false
     },
     "production": {
-        "username": null,
-        "password": null,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
         "dialect": "postgres",
